a059aa6ffdd957bbf8f74a20038b91dc4b59b929
Arslan Mirza <arslantahir0553@gmail.com>
2024-11-11 22:53:51 +0500

    added particles

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,5 +1,90 @@
+import { distance } from "framer-motion";
+import { useCallback } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
 const ParticlesContainer = () => {
-  return <div>Particles Container</div>;
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+  const particlesLoaded = useCallback(async () => {}, []);
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: { enabled: false },
+        background: {
+          color: {
+            value: "",
+          },
+        },
+        fps_limit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+              mode: "push",
+            },
+            onHover: {
+              enable: true,
+              mode: "repulse",
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 90,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#e68e2e",
+          },
+          links: {
+            color: "f5d393",
+            dstance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outModes: {
+              default: "bounce",
+            },
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
 };
 
 export default ParticlesContainer;
--- pages/index.js
+++ pages/index.js
@@ -46,14 +46,14 @@ const Home = () => {
       <div className="w-[1200px] h-full absolute right-0 bottom-0 ">
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
 
-        <div>Particles</div>
+        <ParticlesContainer />
         <motion.div
           variants={fadeIn("up", 0.5)}
           initial="hidden"
           animate="show"
           transition={{ duration: 1, ease: "easeInOut" }}
           exit="hidden"
-          className="w-full h-full max-w-[527px] max-h-[588px] absolute lg:bottom-0 lg:right-[8%]"
+          className="w-full h-full max-w-[357px] max-h-[488px] absolute lg:bottom-0 lg:right-[12%]"
         >
           <Avatar />
         </motion.div>
