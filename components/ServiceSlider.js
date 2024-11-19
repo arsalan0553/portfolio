327c39b5ec09b1f48be886c5a8bbd141e471b47f
Arslan Mirza <arslantahir0553@gmail.com>
2024-11-20 02:07:03 +0500

    feat: added services section

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,44 +1,92 @@
 // icons
+import { Swiper, SwiperSlide } from "swiper/react";
 import {
   RxCrop,
   RxPencil2,
   RxDesktop,
   RxReader,
   RxRocket,
+  RxArrowTopRight,
 } from "react-icons/rx";
-
-
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { FreeMode, Pagination } from "swiper";
+import { m } from "framer-motion";
 // data
 const serviceData = [
   {
     icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Branding",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Development",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Copywriting",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "SEO",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
 ];
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+      }}
+      freeMode={true}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[FreeMode, Pagination]}
+      className="h-[240px] sm:h-[340px]"
+    >
+      {serviceData.map((item, index) => (
+        <SwiperSlide key={index}>
+          <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 group cursor-pointer hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300">
+            <div className="text-4xl text-accent mb-4">{item.icon}</div>
+
+            <div className="mb-8 ">
+              <div className="mb-2 text-lg">{item.title}</div>
+              <p className="max-w-[350px] leading-normal">{item.description}</p>
+            </div>
+            <div className="text-3xl">
+              <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300" />
+            </div>
+          </div>
+        </SwiperSlide>
+
+        // or you can use the original Swiper component if you prefer
+
+        // <SwiperSlide key={index}>
+        //   <div className="p-6 text-white bg-[#212121] rounded-md shadow-md flex flex-col items-center gap-y-4">
+        //     {item.icon}
+        //     <h3 className="text-sm font-semibold">{item.title}</h3>
+      ))}
+    </Swiper>
+  );
 };
 
 export default ServiceSlider;
